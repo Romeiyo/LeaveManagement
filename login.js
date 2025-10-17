496953826a8772daebe9fc05368033e1aea9b312
Romeiyo <romeopomeo1@gmail.com>
2025-10-17 14:23:53 +0200

    User Based Login Works

--- login.js
+++ login.js
@@ -5,6 +5,11 @@ import {
   getAuth,
   signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import{
+  getFirestore,
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,29 +29,74 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const auth = getAuth();
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 //submit button
 const submit = document.getElementById("login");
+const messageElement = document.getElementById("message")
 submit.addEventListener("click", function (event) {
   event.preventDefault();
 
   //inputs
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  //const role = document.getElementById('role').value;
+  const selectedRole = document.getElementById('role').value;
 
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
-      alert("Logging in");
-      window.location.href = "index.html";
+      //alert("Logging in");
+      //window.location.href = "index.html";
+
+      //return db.collection('users').doc(user.uid).get();
+      return getDoc(doc(db, 'users', user.uid));
       // ...
     })
+    .then((docSnap)=>{
+      if (docSnap.exists()){
+        const userData = docSnap.data();
+        const userRole = userData.role;
+
+        if(userRole === selectedRole){
+          messageElement.textContent = 'Login Successful!';
+          messageElement.style.color = 'green';
+
+          redirectBasedOnRole(userRole);
+        }else{
+          messageElement.textContent = 'Invalid role selected';
+          messageElement.style.color = 'red';
+          auth.signOut();
+        }
+      }else{
+        messageElement.textContent = 'User data not found!';
+        messageElement.style.color = 'red';
+      }
+    })
+
+
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       alert(errorMessage)
     });
 });
+
+//redirect users based on role
+function redirectBasedOnRole(role){
+  setTimeout(() => {
+    switch(role){
+      case "Supervisor":
+        //alert("Supervisor Logging in");
+        window.location.href = 'supervisor.html';
+        break
+      case 'HOD':
+        //alert("HOD Logging in");
+        window.location.href = 'hod.html';
+        break
+      default:
+        window.location.href = 'default.html';
+    }
+  }, 1000);
+}
